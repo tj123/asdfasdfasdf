10d9c0a155c648d508c27d424b0c349f8d9671e5
tj123 <tang123jian@126.com>
2016-05-08 19:33:53 +0800

    出问题了 卧槽

--- src/test/java/com/shundian/red/MysqlTest.java
+++ src/test/java/com/shundian/red/MysqlTest.java
@@ -1,10 +1,11 @@
 package com.shundian.red;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shundian.red.lib.dao.DbTemplate;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -12,7 +13,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("file:src/main/resources/spring/spring-dbcp2.xml")
+@ContextConfiguration({"file:src/main/resources/spring/spring-dbcp2.xml", "file:src/main/resources/spring/spring-properties.xml"})
 public class MysqlTest {
 
 	@Autowired
@@ -27,5 +28,14 @@ public class MysqlTest {
 	public void createTable(){
 		template.update("create table user(guid varchar(255),username varchar(255),password varchar(255),sex varchar(255))");
 	}
-	
+
+	@Test
+	public void list(){
+		try {
+			System.out.println(new ObjectMapper().writeValueAsString(template.queryForList("select * from user")));
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
